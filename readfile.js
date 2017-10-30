447ac1a8aad93392cdeeb1c5e925314418f0c034
Gregory Neal <gnealsr@gmail.com>
2017-10-30 15:48:19 -0500

    Completed exercise to read a file

--- readfile.js
+++ readfile.js
@@ -19,3 +19,30 @@ Example output for error on non-existent file:
   $ node cap_file.js
   filename: blah.txt
   ENOENT: no such file or directory, open 'blah.txt'
+*/
+
+// Prep for User Input
+var readline = require('readline');
+var r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Requirement #1 - Prompt the User for a file name
+r1.question("filename: ", function(filename) {
+  // Requirement #2 - Read the contents of the file
+  var fs = require('fs');
+  fs.readFile(filename, function (error, buffer) {
+    if (error) {
+      // Requirement #5
+      console.error(error.message);
+      return
+    }
+    // Requirement #3 - Convert the text to all caps
+    var file_contents = buffer.toString();
+    var uppercase_contents = file_contents.toUpperCase();
+    // Requirement #4 - Print the text
+    console.log(uppercase_contents);
+  });
+  r1.close();
+});
